60f97c52aa03db41dcb5195af6196675650fa203
Kostya_Naydenko <110126766+KostyaNaydenko@users.noreply.github.com>
2024-12-03 10:36:49 +0300

    Revert "Feature blog"

--- admin/src/routes/ProtectedRoute.js
+++ admin/src/routes/ProtectedRoute.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Navigate } from 'react-router-dom';
+
 import useAuth from '../hooks/useAuth';
 
 const ProtectedRoute = ({ component: Component }) => {
-    const { isAuthenticated } = useAuth();
-    //return isAuthenticated ? <Component /> : <Navigate to="/login" />;
-    return <Component/>
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Component /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
